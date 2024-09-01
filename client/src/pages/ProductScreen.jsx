c1bba0e9b4f92b8f1dd9a2e6688aa58e3be35088
teresa <teresa.mdneto@gmail.com>
2024-09-02 00:23:07 +0100

    latest

--- client/src/pages/ProductScreen.jsx
+++ client/src/pages/ProductScreen.jsx
@@ -3,14 +3,31 @@ import { useParams } from "react-router-dom"
 import {Link} from 'react-router-dom'
 import Rating from '../components/Rating';
 import products from '../products';
+import { Row, Col, Image, ListGroup, Card} from 'react-bootstrap';
 
 const ProductScreen = () => {
     const { id : productId } = useParams();
     const product = products.find((p)=>p._id===productId);
-    console.log(product);
+
   return (
-    <div className="min-h-screen p-4">
-      <Link className="btn btn-outline btn-primary">Go Back</Link>
+    <div className="min-h-screen justify-center p-4">
+      <Link className="btn btn-outline mb-4 btn-primary">Go Back</Link>
+     <Row >
+        <Col md={5}>
+        <Image className="center-block p-12 rounded mx-auto d-block;" style={{ width: '500px', height: '500px' } }src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col lassName="mx-auto d-block" md={4}>
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h3 className='text-center'>{product.name}</h3>
+                </ListGroup.Item>
+            </ListGroup>
+
+        </Col>
+        <Col md={3}>
+        </Col>
+     </Row>
+      
     </div>
   )
 }
