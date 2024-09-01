31083026bb3cdbe0a4c10317a0f2f7f7dd3618a9
teresa <teresa.mdneto@gmail.com>
2024-09-01 23:02:13 +0100

    latest

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,19 @@ import Footer from "./components/layout/Footer"
 import Navbar from './components/layout/Navbar'
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Home from './pages/Home'
+import ProductScreen from './pages/ProductScreen'
 
 const App = () => {
   return (
     <Router>
          <div className="flex flex-col justify-between min-h-screen ">
       <Navbar/>
-      <Home/>
+      <main>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/product/:id" element={<ProductScreen/>}/>
+      </Routes>
+      </main>
     </div>
      <Footer/>
     </Router>
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Rating from './Rating';
+import {Link } from 'react-router-dom';
 
 const Product = ({product}) => {
   return (
-    <a href="#" className="group">
+    <Link to={`/product/${product._id}`} className="group">
     <div className="aspect-h-1 product-title aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
       <img src={product.image} className="h-full w-full object-cover object-center group-hover:opacity-75"/>
     </div>
@@ -11,7 +12,7 @@ const Product = ({product}) => {
     <h3 className="mt-4 text-sm text-gray-700">{product.description}</h3>
     <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
     <p className="mt- text-lg font-medium text-gray-900">{product.price}</p>
-  </a>
+  </Link>
   )
 }
 
