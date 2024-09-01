113d4e6145e5ddc9831b6555a449eb55e4a1d240
teresa <teresa.mdneto@gmail.com>
2024-09-01 23:37:48 +0100

    latest

--- client/src/pages/ProductScreen.jsx
+++ client/src/pages/ProductScreen.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useParams } from "react-router-dom"
+import {Link} from 'react-router-dom'
+import Rating from '../components/Rating';
 import products from '../products';
 
 const ProductScreen = () => {
@@ -7,8 +9,8 @@ const ProductScreen = () => {
     const product = products.find((p)=>p._id===productId);
     console.log(product);
   return (
-    <div>
-      ProductScreen
+    <div className="min-h-screen p-4">
+      <Link className="btn btn-outline btn-primary">Go Back</Link>
     </div>
   )
 }
