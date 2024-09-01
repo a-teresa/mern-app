145a6cb2bc1da0dafe504778755c2fed6142e133
teresa <teresa.mdneto@gmail.com>
2024-09-01 01:26:58 +0100

    latest

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,21 +7,21 @@ function Home() {
   console.log(prod);
   return (
     <>
-        <header className='flex flex-col min-h-screen '><img id="fruit" src={fruits}/>
+        <header className='flex flex-col w-full min-h-screen overflow-hidden '><img id="fruit" src={fruits}/>
         <h1 className='text-3xl text-center font-semibold text-black my-7'>Latest Products</h1>
-        
         <div className="bg-white">
           <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-               <a href="#" className="group">
+            {products.map(product => ( 
+               <a key={product._id} href="#" className="group">
                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                 <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+                 <img src={product.image}/>
                </div>
-               <h3 className="mt-4 text-sm text-gray-700">name</h3>
-               <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
-             </a>
+               <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+               <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+             </a>))}
               <a href="#" className="group">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <div className="aspect-h-1 aspect-w-1 w-full rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                   <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
                 </div>
                 <h3 className="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
