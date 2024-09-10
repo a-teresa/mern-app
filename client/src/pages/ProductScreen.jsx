5e83917cff4cb9e287096c3370fd077479acad85
teresa <teresa.mdneto@gmail.com>
2024-09-10 15:39:10 +0100

    fetch completed

--- client/src/pages/ProductScreen.jsx
+++ client/src/pages/ProductScreen.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import { useParams } from "react-router-dom"
 import {Link} from 'react-router-dom'
 import Rating from '../components/Rating';
-import products from '../products';
+
 import { Row, Col, Image, ListGroup, Card, ListGroupItem, Button} from 'react-bootstrap';
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({});
     const { id : productId } = useParams();
-    const product = products.find((p)=>p._id===productId);
+    
+
+    useEffect(() => {
+        fetchProduct();
+    }, [productId])
+
+    const fetchProduct = async () =>{
+        const data = await fetch(`/api/products/${productId}`);      
+        const p = await data.json();  
+        setProduct(p);
+      }  
 
   return (
     <div className="min-h-screen justify-center p-4">
@@ -16,7 +27,7 @@ const ProductScreen = () => {
         <Col md={5}>
         <Image className="center-block p-12 rounded mx-auto d-block;" style={{ width: '500px', height: '500px' } }src={product.image} alt={product.name} fluid/>
         </Col>
-        <Col lassName="mx-auto d-block" md={4}>
+        <Col className="mx-auto d-block" md={4}>
             <ListGroup variant='flush'>
                 <ListGroup.Item>
                     <h3 className='text-center'>{product.name}</h3>
