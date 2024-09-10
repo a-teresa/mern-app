c2a20ac70bf7f3a29d051c368c8703eea7a8a2f8
teresa <teresa.mdneto@gmail.com>
2024-09-10 15:26:08 +0100

    latest fetching products from sertver

---
+++ api/data/products.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Carrots',
+    image: '/images/carrots.png',
+    description:
+      'Satisfying crunch alongside a mildly sweet and earthy flavor.',
+    category: 'food',
+    price: 0.99,
+    countInStock: 100,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Apples',
+    image: '/images/apples.png',
+    description:
+      'Extremely aromatic with sweet, rich flavor, and very juicy',
+    category: 'food',
+    price: 1.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Oranges',
+    image: '/images/oranges.png',
+    description:
+      'Extremely aromatic with sweet, rich flavor, and very juicy. ',
+    brand: 'Cannon',
+    category: 'food',
+    price: 1.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Pears',
+    image: '/images/pears.png',
+    description:
+      'Extremely aromatic with sweet, rich flavor, and very juicy. ',
+    category: 'food',
+    price: 2.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Bread',
+    image: '/images/bread.png',
+    description:
+      'Quality bread freshly baked.',
+    category: 'food',
+    price: 5.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Coffee',
+    image: '/images/coffee.png',
+    description:
+      'To create homemade lattes, fresh espresso or a simple pot of drip coffee.',
+    category: 'food',
+    price: 9.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
-import fruits from '../assets/fruits.png'
-import Product from '../components/Product'
-import products from '../products'
+import {useEffect, useState} from 'react';
+import fruits from '../assets/fruits.png';
+import Product from '../components/Product';
+import axios from 'axios';  
 
 
-function Home() {
-  const prod = products;
-  console.log(prod);
-  return (
-    <>
-        <header className='flex flex-col w-full min-h-screen overflow-hidden '><img id="fruit" src={fruits}/>
-        <h1 className='text-3xl text-center font-semibold text-black my-7'>Latest Products</h1>
-        <div className="bg-white">
-          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.map(product => ( 
-             <Product key={product._id}  product={product}/>
-             ))}
-             
-            </div>
-          </div>
-        </div>
 
-        </header>
-     
+  function Home() {
+    const [products, setProducts] = useState([]);
+  
+
+    useEffect(() => {
+      fetchProducts();
+    }, []);  
 
     
-    </>
+    const fetchProducts = async () =>{
+      const data = await fetch('/api/products');  
+      const p = await data.json();  
+      setProducts(p);
+    }
+    return (
+      <>
+          <header className='flex flex-col w-full min-h-screen overflow-hidden '><img id="fruit" src={fruits}/>
+          <h1 className='text-3xl text-center font-semibold text-black my-7'>Latest Products</h1>
+          <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map(product => (
+                
+              <Product key={product._id} product={product}/>
+  ))}
+              
+              </div>
+            </div>
+          </div>
+
+          </header>
+      
+
+      
+      </>
   )
 }
 
